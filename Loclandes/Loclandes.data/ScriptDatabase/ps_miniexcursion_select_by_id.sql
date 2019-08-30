da04f6d40d6cea840df3b0940de70bbe15914b04
Koulikana <48823375+Koulikana@users.noreply.github.com>
2019-08-30 12:40:40 +0200

    "If exists -> drop" condition added

--- Loclandes/Loclandes.data/ScriptDatabase/ps_miniexcursion_select_by_id.sql
+++ Loclandes/Loclandes.data/ScriptDatabase/ps_miniexcursion_select_by_id.sql
@@ -1,3 +1,7 @@
+IF OBJECT_ID('ps_miniexcursion_select_by_id', 'P') IS NOT NULL
+DROP PROC ps_miniexcursion_select_by_id
+GO
+
 CREATE PROCEDURE ps_miniexcursion_select_by_id
 (
 	@idMiniExcursion int
