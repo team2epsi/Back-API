2a469e5de344a8d0fd2e2613812ab06aa7d0cbde
Koulikana <48823375+Koulikana@users.noreply.github.com>
2019-08-30 12:35:03 +0200

    Stored procedure changed

--- Loclandes/Loclandes.data/ScriptDatabase/ps_miniexcursion_update.sql
+++ Loclandes/Loclandes.data/ScriptDatabase/ps_miniexcursion_update.sql
@@ -1,3 +1,7 @@
+IF OBJECT_ID('ps_miniexcursion_update', 'P') IS NOT NULL
+DROP PROC ps_miniexcursion_update
+GO
+
 CREATE PROCEDURE ps_miniexcursion_update
 (
 	@idMiniExcursion int,
@@ -6,11 +10,21 @@ CREATE PROCEDURE ps_miniexcursion_update
 )
 
 AS
+
+declare @mini int
+set @mini = (SELECT count(*) FROM MiniExcursions WHERE idMiniExcursion = @idMiniExcursion)
+print @mini
+
+IF (@mini = 1)
 BEGIN
 
 UPDATE dbo.[MiniExcursions]
 SET [libelleMiniExcursion] = @libelleMiniExcursion,
 	[nombrePlaceMiniExcursion] = @nombrePlaceMiniExcursion
 WHERE [idMiniExcursion] = @idMiniExcursion
-
+print 'MiniExcursion has been successfully updated'
 END
+ELSE
+print 'MiniExcursion has not been updated'
+
+--EXEC ps_miniexcursion_update_test @idMiniExcursion=23, @libelleMiniExcursion='coucou', @nombrePlaceMiniExcursion = 2
